afd7828901a06684abb21b79cf708b3518616313
Msarbanian <maedeh.sarbanian@gmail.com>
2024-10-27 14:38:12 +0330

    Done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import HelloWorld from "./HelloWorld.jsx";
+import Card from "./Card.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
